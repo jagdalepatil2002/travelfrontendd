463f126f9a958c684341b01e69fb5e435a5e1618
Tejas Jagdale <132334039+jagdalepatil2002@users.noreply.github.com>
2025-08-29 23:03:50 +0530

    Change API base URL to local development server

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const listenButton = document.getElementById('listen-button');
     const placeDescription = document.getElementById('place-description');
 
-    const API_BASE_URL = 'https://backendtravel-pgzt.onrender.com/';
+    const API_BASE_URL = 'http://127.0.0.1:5000';
 
     let isPlaying = false;
     let currentAudio;
